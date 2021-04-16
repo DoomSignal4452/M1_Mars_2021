765aa02b1faaefb2f7a9b19942ce65ba41fb5719
DoomSignal4452 <76997074+DoomSignal4452@users.noreply.github.com>
2021-04-16 12:51:23 +0200

    upit

---
+++ Week4/Oppgave_2/lib.js
@@ -0,0 +1,35 @@
+function dotBefore(s) {
+    let atIndex=s.indexOf("@")
+    let dotIndex=s.indexOf(".")
+
+    if (dotIndex<atIndex && dotIndex>=0) {
+        return true;
+    } else {return false;}
+}
+
+function dotAfter(s) {
+    let atIndex=s.indexOf("@")
+    let dotIndex=s.lastIndexOf(".")
+
+    if (dotIndex>atIndex) {
+        return true;
+    } else {return false;}
+}
+
+function isitemail(s) {
+    mayBeEmail=s;
+    if (mayBeEmail.includes("@") && dotBefore(s)==true && dotAfter(s)==true) {
+        if (mayBeEmail.includes(" ")) {
+            infoTXT="is not an email.";
+            return false;
+        } else {
+            infoTXT="is an email.";
+            return true;
+        }
+    } else {
+        infoTXT="is not an email.";
+        return false;
+    }
+
+    updateView();
+}
